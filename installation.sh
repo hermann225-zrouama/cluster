6bd7ab9a541bdd801a1ad1312c9b2350e351ee3c
ZROUAMA FRANCK HERMANN DEBAH <debah2002@gmail.com>
2025-06-12 14:12:17 +0000

    amoaman cluster erp-dev

--- installation.sh
+++
@@ -1,5 +0,0 @@
-# clickhouse
-helm -n clickhouse install clickhouse oci://registry-1.docker.io/bitnamicharts/clickhouse
-echo "Username: default"
-echo "Password: $(kubectl get secret --namespace clickhouse clickhouse -o jsonpath="{.data.admin-password}" | base64 -d)"
-wXr5SXFpXM
