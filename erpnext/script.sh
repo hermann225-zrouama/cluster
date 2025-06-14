7537d5e0f036c09a0abb62a90dce280ab75212c6
ZROUAMA FRANCK HERMANN DEBAH <debah2002@gmail.com>
2025-06-14 12:35:47 +0000

    amoaman cluster erp-sith

--- erpnext/script.sh
+++ erpnext/script.sh
@@ -5,6 +5,9 @@ helm install frappe-bench -n $NAMESPACE -f values.yaml frappe/erpnext
 # 2- Update ERPNext with Helm
 helm upgrade frappe-bench frappe/erpnext -f values.yaml -n $NAMESPACE
 kubectl -n $NAMESPACE exec -it erpnext-gunicorn-xxxx -- bash
+
+# bench install-app erpnext
+bench --site $SITE_NAME install-app hrms
 # execute bench migrate commands in the container
 bench --site $SITE_NAME migrate --skip-failing
 
