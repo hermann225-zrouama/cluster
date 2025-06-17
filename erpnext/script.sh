9f9689afd68228ac77a10955b555318f445455a8
ZROUAMA FRANCK HERMANN DEBAH <debah2002@gmail.com>
2025-06-17 14:30:43 +0000

    add transport

--- erpnext/script.sh
+++ erpnext/script.sh
@@ -1,11 +1,11 @@
 # 1- Install Helm
-helm install frappe-bench -n $NAMESPACE -f values.yaml frappe/erpnext
+helm install frappe-bench -n erp-test -f values.yaml frappe/erpnext
 
 # 2- Update ERPNext with Helm
-helm upgrade frappe-bench frappe/erpnext -f values.yaml -n $NAMESPACE
-kubectl -n $NAMESPACE exec -it erpnext-gunicorn-xxxx -- bash
+helm upgrade frappe-bench frappe/erpnext -f values.yaml -n erp-test
+kubectl -n erp-test exec -it erpnext-gunicorn-xxxx -- bash
 # execute bench migrate commands in the container
 bench --site $SITE_NAME migrate --skip-failing
 
 # 3- Uninstall ERPNext with Helm
-helm uninstall frappe-bench -n $NAMESPACE
+helm uninstall frappe-bench -n erp-test
